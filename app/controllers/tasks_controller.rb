22d8176833d31abc2637e741532124a354b06e04
takenobu.seo <hawk572white@gmail.com>
2021-03-22 21:02:08 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   end
   
   def show
-    @task = Task.find(params[:id])
+    set_task
   end
   
   def new
@@ -24,11 +24,11 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+    set_task
   end
   
   def update
-    @task = Task.find(params[:id])
+    set_task
     
     if @task.update(task_params)
       flash[:success] = 'タスクは更新されました。'
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
+    set_task
     @task.destroy
     
     flash[:success] = 'タスクは削除されました'
@@ -50,6 +50,10 @@ class TasksController < ApplicationController
   private
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
