65a72561b2f9bb5022d3ad2656ae4b91c35850c7
takenobu.seo <hawk572white@gmail.com>
2021-03-23 13:31:59 +0000

    Add column remake

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
   
   def show
-    set_task
   end
   
   def new
@@ -24,11 +25,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    set_task
   end
   
   def update
-    set_task
     
     if @task.update(task_params)
       flash[:success] = 'タスクは更新されました。'
@@ -40,7 +39,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    set_task
     @task.destroy
     
     flash[:success] = 'タスクは削除されました'
