8ac2505d72a5db28a7b36f5e0aca5e9d4941158d
takenobu.seo <hawk572white@gmail.com>
2021-04-21 01:23:17 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,23 @@
 class TasksController < ApplicationController
   
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  def index
-    @tasks = Task.all
-  end
+  before_action :require_user_logged_in
+  before_action :set_task, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
   
-  def show
+  def index
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = '正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = '投稿に失敗しました'
       render :new
@@ -31,7 +31,7 @@ class TasksController < ApplicationController
     
     if @task.update(task_params)
       flash[:success] = 'タスクは更新されました。'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'タスクの更新に失敗しました'
       render :edit
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'タスクは削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
   private
@@ -52,6 +52,13 @@ class TasksController < ApplicationController
   end
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
 
-  resources :tasks
+  resources :tasks, only: [:new, :create, :edit, :update, :destroy]
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
